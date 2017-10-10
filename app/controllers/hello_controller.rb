688287ff7fb2d42c0f369aa3cded89ff0c883b67
Mike Dacanay <migd64@gmail.com>
2017-10-09 17:01:25 -0700

    2 out of 7 points done

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -2,4 +2,8 @@ class HelloController < ApplicationController
   def index
   	render "hello/index"
   end
+
+  def say
+  	render "hello/say"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'hello' => "hello#index"
-
+  get  'say/hello' => "hello#say"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
